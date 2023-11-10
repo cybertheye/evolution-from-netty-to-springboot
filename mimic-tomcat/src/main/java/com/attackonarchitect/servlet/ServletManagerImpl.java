dd1fdcefc6c3e990491ae2f0a9539f3b22d153bc
cybertheye <cybertheye@yeah.net>
2023-11-10 16:41:06 +0800

    1.新增 loadOnStartup 初始化功能 2.对@WebInitParams的处理

--- mimic-tomcat/src/main/java/com/attackonarchitect/servlet/ServletManagerImpl.java
+++ mimic-tomcat/src/main/java/com/attackonarchitect/servlet/ServletManagerImpl.java
@@ -4,6 +4,8 @@ import com.attackonarchitect.ComponentScanner;
 import com.attackonarchitect.context.ServletContext;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,19 +14,21 @@ import java.util.Set;
 /**
  * @description:
  */
-public class ServletManagerImpl implements ServletManager{
-    private ServletManagerImpl(){}
+public class ServletManagerImpl implements ServletManager {
+    private ServletManagerImpl() {
+    }
 
     private ComponentScanner provider;
 
     private ServletContext servletContext;
 
 
-    private static class ServletManagerImplHolder{
+    private static class ServletManagerImplHolder {
         static ServletManagerImpl instance = new ServletManagerImpl();
     }
-    static ServletManager getInstance(ComponentScanner provider,ServletContext servletContext){
-        ServletManagerImpl impl =  ServletManagerImplHolder.instance;
+
+    static ServletManager getInstance(ComponentScanner provider, ServletContext servletContext) {
+        ServletManagerImpl impl = ServletManagerImplHolder.instance;
 //        impl.setScanPackages(scanPackages);
         impl.setProvider(provider);
         impl.setServletContext(servletContext);
@@ -33,14 +37,14 @@ public class ServletManagerImpl implements ServletManager{
     }
 
     private void preInit() {
-       this.getServletMapping().put("default",new DefaultMimicServlet());
-       this.getServletMapping().put("error",new ErrorMimicServlet());
+        this.getServletMapping().put("default", new DefaultMimicServlet());
+        this.getServletMapping().put("error", new ErrorMimicServlet());
 
-       //新增 loadOnStartup 初始化功能
+        //新增 loadOnStartup 初始化功能
         Collection<ServletInformation> values = provider.getServletInformationMap().values();
 
-        values.forEach(information->{
-            if(information.getLoadOnStartup()>0){
+        values.forEach(information -> {
+            if (information.getLoadOnStartup() > 0) {
                 instantiate(information);
             }
         });
@@ -51,18 +55,15 @@ public class ServletManagerImpl implements ServletManager{
     //////////////////////
 
 
-
-
-
     //////
 
     /**
      * clazzName - Servlet
      */
-    private Map<String,Servlet> servletMapping;
+    private Map<String, Servlet> servletMapping;
 
     private Map<String, Servlet> getServletMapping() {
-        if(servletMapping == null){
+        if (servletMapping == null) {
             servletMapping = new HashMap<>();
         }
         return servletMapping;
@@ -70,7 +71,7 @@ public class ServletManagerImpl implements ServletManager{
 
     @Override
     public Servlet getSpecifedServlet(String uri) {
-        if(servletMapping.containsKey(uri)){
+        if (servletMapping.containsKey(uri)) {
             return servletMapping.get(uri);
         }
         Map<String, ServletInformation> servletInformationMap = provider.getServletInformationMap();
@@ -83,40 +84,67 @@ public class ServletManagerImpl implements ServletManager{
     }
 
 
-    private Servlet instantiate(ServletInformation servletInformation){
+    /**
+     * 实例化Servlet
+     * @param servletInformation
+     * @return
+     */
+    private Servlet instantiate(ServletInformation servletInformation) {
         String clazzName = servletInformation.getClazzName();
+        Servlet instance = null;
         try {
             if (!servletMapping.containsKey(clazzName)) {
 
                 Class<?> clazz = Class.forName(clazzName);
-                Servlet instance = (Servlet)clazz.newInstance();
-
+                instance = (Servlet) clazz.newInstance();
                 Field servletContextField = null;
 
-                while(true){
+                Class<?> traveler = clazz;
+                while (true) {
                     try {
-                        servletContextField = clazz.getDeclaredField("servletContext");
+                        servletContextField = traveler.getDeclaredField("servletContext");
                         servletContextField.setAccessible(true);
                         break;
-                    }catch (NoSuchFieldException e) {
-                        clazz = clazz.getSuperclass();
+                    } catch (NoSuchFieldException e) {
+                        traveler= traveler.getSuperclass();
                     }
                 }
+                //Field 直接 set，和 通过 setter 方法的区别在于 setter可以有逻辑处理
+                servletContextField.set(instance, servletContext);
 
-                servletContextField.set(instance,servletContext);
-                servletMapping.put(clazzName,instance);
+                // 对于 注解 @WebInitParams 传过来的参数处理
+                Map<String, String> initParams = servletInformation.getInitParams();
+
+                Class<?> finalClazz = clazz;
+                Servlet finalInstance = instance;
+                initParams.forEach((key, value) -> {
+                    try {
+                        String setterName = "set" + key.substring(0, 1).toUpperCase() + key.substring(1);
+                        Method method = finalClazz.getMethod(setterName,value.getClass());
+                        method.invoke(finalInstance,value);
+                    } catch (NoSuchMethodException e) {
+                        throw new RuntimeException(e);
+                    } catch (InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+
+                String[] urlPatterns = servletInformation.getUrlPattern();
+                for(String urlPattern : urlPatterns){
+                    servletMapping.put(urlPattern, instance);
+                }
             }
 
-            return servletMapping.get(clazzName);
+            return instance;
 
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
+        return null;
     }
 
 
@@ -130,11 +158,8 @@ public class ServletManagerImpl implements ServletManager{
 
     @Override
     public Set<String> getAllRequestUri() {
-        Map<String, String> webServletComponents = getProvider().getWebServletComponents();
-        return webServletComponents.keySet();
+        return getProvider().getServletInformationMap().keySet();
     }
-
-
 
 
     /////////getter ,setter
