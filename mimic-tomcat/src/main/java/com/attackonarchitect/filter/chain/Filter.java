c56e437b5027042feac22ed35c297dc41eb62c30
Terrdi <675464934@qq.com>
2023-11-16 16:40:33 +0800

    Fixed #5 通过成员变量实现逐个Filter访问

--- mimic-tomcat/src/main/java/com/attackonarchitect/filter/chain/Filter.java
+++ mimic-tomcat/src/main/java/com/attackonarchitect/filter/chain/Filter.java
@@ -6,9 +6,11 @@ import com.attackonarchitect.http.MTResponse;
 import java.io.UnsupportedEncodingException;
 
 /**
+ * 过滤器
+ *
  * @description:
  */
 
 public interface Filter {
-    boolean doFilter(MTRequest request, MTResponse response) throws UnsupportedEncodingException;
+    boolean doFilter(MTRequest request, MTResponse response, FilterChain filterChain) throws UnsupportedEncodingException;
 }
